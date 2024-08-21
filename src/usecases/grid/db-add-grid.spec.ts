63e60aaa865a952ecfdd6e49a1c37448655183ef
D3C0RU5 <chpeterle.profissional@gmail.com>
2024-08-20 21:59:14 -0300

    feat: ensure DbAddGrid returns snapshot when success

--- src/usecases/grid/db-add-grid.spec.ts
+++ src/usecases/grid/db-add-grid.spec.ts
@@ -1,4 +1,5 @@
 import { Grid } from '../../domain/entities/grid'
+import { Size } from '../../domain/value-objects/size'
 import { IGridRepositoryCreate } from '../protocols/grid/grid-repository-create'
 import { DbAddGrid, Input } from './db-add-grid'
 
@@ -19,23 +20,50 @@ const makeSut = (): SutType => {
   }
 }
 
-const makeInput = (
-  data: { rows: number; columns: number } = { rows: 10, columns: 10 },
-): Input => {
-  const { rows, columns } = data
+const makeInput = (): Input => {
   return {
     size: {
-      rows,
-      columns,
+      rows: 10,
+      columns: 10,
     },
   }
 }
 
+const fakeSize = new Size(10, 5)
+const fakeGrid = new Grid('grid-id', fakeSize)
+
 describe('testing DbAddGrid', () => {
+  beforeEach(() => {
+    jest.spyOn(Size, 'create').mockReturnValue(fakeSize)
+    jest.spyOn(Grid, 'create').mockReturnValue(fakeGrid)
+  })
+
   it('throws if Size.create is invalid', async () => {
     // Arrange
     const { sut } = makeSut()
-    const input = makeInput({ rows: -1, columns: -2 })
+    const input = makeInput()
+
+    // Mock
+    jest.spyOn(Size, 'create').mockImplementation(() => {
+      throw new Error('any error')
+    })
+
+    // Act
+    const execution = sut.execute(input)
+
+    // Assert
+    await expect(() => execution).rejects.toThrow('any error')
+  })
+
+  it('throws if Grid.create is invalid', async () => {
+    // Arrange
+    const { sut } = makeSut()
+    const input = makeInput()
+
+    // Mock
+    jest.spyOn(Grid, 'create').mockImplementation(() => {
+      throw new Error('any error')
+    })
 
     // Act
     const execution = sut.execute(input)
@@ -44,15 +72,19 @@ describe('testing DbAddGrid', () => {
     await expect(() => execution).rejects.toThrow()
   })
 
-  it('throws if Grid.create is invalid', async () => {
+  it('call grid create if valid', async () => {
     // Arrange
-    const { sut } = makeSut()
-    const input = makeInput({ rows: -1, columns: -2 })
+    const { sut, createRepositoryStub } = makeSut()
+    const input = makeInput()
+
+    // Mock
+    const createSpy = jest.spyOn(createRepositoryStub, 'create')
 
     // Act
-    const execution = sut.execute(input)
+    const result = await sut.execute(input)
 
     // Assert
-    await expect(() => execution).rejects.toThrow()
+    expect(createSpy).toHaveBeenCalledWith(fakeGrid)
+    expect(result).toEqual(fakeGrid.toSnapshot())
   })
 })
