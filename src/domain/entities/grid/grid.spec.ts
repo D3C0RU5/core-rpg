1c486484a4486870addb9e8e93785ed454c21f9f
D3C0RU5 <chpeterle.profissional@gmail.com>
2024-09-09 21:52:59 -0300

    test: implement tests for entity Grid

--- src/domain/entities/grid/grid.spec.ts
+++ src/domain/entities/grid/grid.spec.ts
@@ -2,6 +2,22 @@ import { Grid } from '.'
 import { Size } from '../../value-objects/size'
 
 describe('Grid testes', () => {
+  describe('contructor unit test', () => {
+    it('Return instance', () => {
+      // Arrange
+      const uuid = crypto.randomUUID()
+      const size = Size.create(1, 2)
+
+      // Assert
+      const grid = new Grid(uuid, size)
+
+      // Assert
+      expect(grid).toBeInstanceOf(Grid)
+      expect(grid['gridId']).toBe(uuid)
+      expect(grid['size'].toSnapshot()).toEqual(size.toSnapshot())
+    })
+  })
+
   describe('create unit test', () => {
     it('Return instance', () => {
       // Assert
@@ -12,13 +28,48 @@ describe('Grid testes', () => {
     })
   })
 
-  describe('recover unit test', () => {
-    it('Should recover grid instance', () => {
-      // Assert
-      const grid = Grid.create(Size.create(1, 2))
+  describe('getId unit test', () => {
+    it('Should return uuid value', () => {
+      // Arrange
+      const uuid = crypto.randomUUID()
+      const size = Size.create(1, 2)
 
       // Assert
-      expect(grid).toBeInstanceOf(Grid)
+      const grid = new Grid(uuid, size)
+
+      // Assert
+      expect(grid.getId()).toBe(uuid)
+    })
+  })
+
+  describe('getSize unit test', () => {
+    it('Should return size value', () => {
+      // Arrange
+      const uuid = crypto.randomUUID()
+      const size = Size.create(1, 2)
+
+      // Assert
+      const grid = new Grid(uuid, size)
+
+      // Assert
+      expect(grid.getSize()).toBe(size)
+    })
+  })
+
+  describe('toSnapshot unit test', () => {
+    it('Should recover grid instance', () => {
+      // Arrange
+      const uuid = crypto.randomUUID()
+      const size = Size.create(1, 2)
+
+      // Assert
+      const grid = new Grid(uuid, size)
+
+      // Assert
+      expect(grid.toSnapshot()).toEqual({
+        gridId: uuid,
+        size: size.toSnapshot(),
+      })
     })
   })
 })
